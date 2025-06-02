da6c4580d59e43edf112a38cae96c31b31e357d4
Abhishek <abhisheks@pearlorganisation.com>
2025-06-02 11:34:00 +0530

    secretkey removed

--- utils/encryption.js
+++ utils/encryption.js
@@ -12,38 +12,47 @@ const SECRET_KEY = process.env.SESSION_SECRET || 'ebook-secret-key';
 const ENCRYPTION_KEY = getKey(SECRET_KEY);
 const IV_LENGTH = 16;
 
-// Encryption
 function encrypt(text) {
-    try {
-        const iv = crypto.randomBytes(IV_LENGTH);
-        const cipher = crypto.createCipheriv('aes-256-cbc', ENCRYPTION_KEY, iv);
-        let encrypted = cipher.update(text);
-        encrypted = Buffer.concat([encrypted, cipher.final()]);
-        return iv.toString('hex') + ':' + encrypted.toString('hex');
-    } catch (error) {
-        console.error('Encryption error:', error);
-        throw error;
-    }
+    return text; // no encryption
 }
 
-// Decryption
 function decrypt(text) {
-    try {
-        const textParts = text.split(':');
-        if (textParts.length !== 2) {
-            throw new Error('Invalid encrypted text format');
-        }
-        const iv = Buffer.from(textParts[0], 'hex');
-        const encryptedText = Buffer.from(textParts[1], 'hex');
-        const decipher = crypto.createDecipheriv('aes-256-cbc', ENCRYPTION_KEY, iv);
-        let decrypted = decipher.update(encryptedText);
-        decrypted = Buffer.concat([decrypted, decipher.final()]);
-        return decrypted.toString();
-    } catch (error) {
-        console.error('Decryption error:', error);
-        throw error;
-    }
+    return text; // no decryption
 }
+
+
+// Encryption
+// function encrypt(text) {
+//     try {
+//         const iv = crypto.randomBytes(IV_LENGTH);
+//         const cipher = crypto.createCipheriv('aes-256-cbc', ENCRYPTION_KEY, iv);
+//         let encrypted = cipher.update(text);
+//         encrypted = Buffer.concat([encrypted, cipher.final()]);
+//         return iv.toString('hex') + ':' + encrypted.toString('hex');
+//     } catch (error) {
+//         console.error('Encryption error:', error);
+//         throw error;
+//     }
+// }
+
+// // Decryption
+// function decrypt(text) {
+//     try {
+//         const textParts = text.split(':');
+//         if (textParts.length !== 2) {
+//             throw new Error('Invalid encrypted text format');
+//         }
+//         const iv = Buffer.from(textParts[0], 'hex');
+//         const encryptedText = Buffer.from(textParts[1], 'hex');
+//         const decipher = crypto.createDecipheriv('aes-256-cbc', ENCRYPTIKEY, iv);
+//         let decrypted = decipher.update(encryptedText);
+//         decrypted = Buffer.concat([decrypted, decipher.final()]);
+//         return decrypted.toString();
+//     } catch (error) {
+//         console.error('Decryption error:', error);
+//         throw error;
+//     }
+// }
     
 module.exports = {
     encrypt,
